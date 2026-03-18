053d9ff136bb9ff1d2ebfec9a774d5dbea3e43dc
heart <a0966188a@cmail.com>
2026-03-18 15:32:57 +0800

    股市2

---
+++ stock2.py
@@ -0,0 +1,31 @@
+import schedule
+import time
+from datetime import datetime
+
+def run_every_5seconds():
+    print(f"每 5秒執行一次")
+
+def run_every_60seconds():
+    print(f"每 60秒執行一次")
+
+def run_at_spec_time():
+    now=datetime.now()
+    print(f"觸發時間:{now}")
+def run_and_cancelJob():
+    cnt=True
+    if(cnt):
+        return schedule.cancel_job
+
+schedule.every(5).seconds.do(run_every_5seconds)
+schedule.every(1).seconds.do(run_every_60seconds)
+schedule.every().day.at("14:00").do(run_at_spec_time)
+
+schedule.every(30).seconds.do(run_every_5seconds).unit("13:40")
+schedule.every().hours.at(":50").do(run_and_cancelJob)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
+
+
